3f7a1380c721da8fe5913e2d67f721c5d6e59ffe
JanusKruzus <cernykapesnik@seznam.cz>
2024-03-22 17:15:31 +0100

    random kanji with readings and redisign

--- js/randomKanji.js
+++ js/randomKanji.js
@@ -1,34 +1,57 @@
 $(document).ready(function() {
+
+    //API URLs
     let JOYO_KANJI_URL = "https://kanjiapi.dev/v1/kanji/joyo"
     let KANJI_INFO_URL = "https://kanjiapi.dev/v1/kanji/";
+
     let kanjiRendered = false;
-    $("#get_rand").click(function() {
-        let element = document.getElementById("kanjiTitle")
+
+    $("#getRand").click(function() {
+        let element = $(".kanjiTitle")
+
+        //refreshes generated kanji info
         if(element !== null) {
-            $("#kanjiTitle").remove()
-            $("#meanings").remove()
+            $(".kanjiTitle").remove()
+            $(".meanings").remove()
+            $(".readings").remove()
         }
-        
+
             $.getJSON(JOYO_KANJI_URL, function(kanjiList) {
                 let randomInt = Math.floor(Math.random() * kanjiList.length) + 1
-                let kanjiResult = KANJI_INFO_URL + kanjiList[randomInt]
-                console.log(kanjiResult)
-                $.getJSON(kanjiResult, function(data){
-                    let kanji = data.kanji
-                    let meanings = data.meanings
+                let kanjiResult = KANJI_INFO_URL + kanjiList[randomInt] //random joyo kanji
 
-                    $("#get_rand").after('<p id="kanjiTitle">'+kanji+'</p>')
-                    $("#kanjiTitle").after('<ul id="meanings"></ul>')
-    
+                //get specific kanji data
+                $.getJSON(kanjiResult, function(data){
+
+                    let kanji = data.kanji
+                    
+                    //arrays, slice to print max 3 values, that should be enough
+                    let meanings = data.meanings.slice(0,3) 
+                    let onReading = data.on_readings.slice(0,3)
+                    let kunReadings = data.kun_readings.slice(0,3)
+
+                    $("#getRand").after('<p class="kanjiTitle">'+kanji+'</p>')
+                    $(".kanjiTitle").after('<div class="meanings" id="meaningContainer"></div>')
+                    $(".meanings").after('<div class="readings" id="kunContainer"></div>')
+                    $("#kunContainer").after('<div class="readings" id="onContainer"></div>')
+
+                    $("#meaningContainer").append('<span class="spanTitle">Meanings</span>')
+                    $("#kunContainer").append('<span class="spanTitle">Kun</span>')
+                    $("#onContainer").append('<span class="spanTitle">On</span>')
+
+
+                    //filling the meanings and readings
                     meanings.forEach(el => {
-                        $("#meanings").append("<li>"+el+"</li>")
+                        $("#meaningContainer").append("<span>"+el+"</span>")
                     });
-     
+                    kunReadings.forEach(el => {
+                        $("#kunContainer").append("<span>"+el+"</span>")
+                    });
+                    onReading.forEach(el => {
+                        $("#onContainer").append("<span>"+el+"</span>")
+                    });
+
                 })
             })
-        
-
-        
-
     })
 })
